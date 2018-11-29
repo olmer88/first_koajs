eab38bed4c6742451a7d29019a2beda762a0d88f
max <maksym.zhuk@ria.com>
2018-11-29 11:37:26 +0200

    lesson 3

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,22 @@
+const _ = require('lodash');
 const generateId = require('uniq-id');
-let posts = {};
+
+let posts = {
+  1: { title: 'First post', content: 'Content from first post' },
+  2: { title: 'Second post', content: 'Content from second post' },
+};
 
 async function indexAction(ctx) {
-  ctx.body = posts;
+  const viewVariables = {
+    posts,
+    title: 'list posts',
+    _,
+  };
+  await ctx.render('index', viewVariables);
 }
 
 async function newPostAction(ctx) {
-  await ctx.render('newPost', { a: 1 });
+  await ctx.render('newPost', { title: 'new post' });
 }
 
 async function savePostAction(ctx) {
@@ -18,8 +28,17 @@ async function savePostAction(ctx) {
   ctx.redirect('/');
 }
 
+async function viewPostAction(ctx) {
+  // const { id } = ctx.request.query;
+  const { id } = ctx.params;
+  ctx.body = posts[id];
+}
+
+
+
 module.exports = {
   indexAction,
   newPostAction,
   savePostAction,
+  viewPostAction,
 };
--- routes.js
+++ routes.js
@@ -1,13 +1,16 @@
 const router = require('koa-router')();
 const {
-  indexAction, newPostAction, savePostAction
+  indexAction, newPostAction, savePostAction, viewPostAction,
 } = require('./controllers/indexController');
 
 router
   .get('/', indexAction) // list all posts titles
-// .get('/post/:id', show) // show one post
+  .get('/post/:id', viewPostAction) // show one post
   .get('/post/new', newPostAction) // create new post
-.post('/post/save', savePostAction) // url to submit form
+  .post('/post/save', savePostAction) // url to submit form
+  .all('**', async (ctx) => {
+    ctx.body = 'Sorry can\'t find route';
+  })
 ;
 
 module.exports = router.routes();
